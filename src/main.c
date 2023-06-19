499cfb814a7fe6903b6c21baeea2ca1e3aea8da7
ElPatatone <rahman.hm2002@gmail.com>
2023-06-19 21:22:27 +0100

    feat: improving the storing of workout info

--- src/main.c
+++ src/main.c
@@ -1,17 +1,27 @@
 #include <ncurses.h>
 #include <string.h>
 
+#define MAX_WORKOUTS 365
+
 void center(char *title);
+void welcomeScreen();
+
+typedef struct Workout {
+
+    char date[10];
+    char training[10];
+    char time[10];
+
+} Workout;
 
 int main(void)
 {
-    char date[10];
+    Workout workout1[MAX_WORKOUTS];
+    int workoutNum = 0;
 
     initscr();  // Initialize the ncurses library
-    attron(A_BOLD);
-    center("Hello, ready to lift some weights?");
-    refresh();
-    getch();
+
+    welcomeScreen();
 
     clear();
     addstr("ALLENO-ORA (TRAIN NOW)\n");
@@ -19,8 +29,17 @@ int main(void)
 
     move(3, 0);
     addstr("Make new workout entry for date: ");
-    getstr(date);
-    printw("You have chosen %s as the date", date);
+    getstr(workout1[workoutNum].date);
+    printw("You have chosen %s as the date\n", workout1[workoutNum].date);
+
+    addstr("What did you train: ");
+    getstr(workout1[workoutNum].training);
+    printw("You trained %s.\n", workout1[workoutNum].training);
+
+    addstr("How long was the workout: ");
+    getstr(workout1[workoutNum].time);
+    printw("You trained for %s.\n", workout1[workoutNum].time);
+
     refresh();
     getch();
     endwin();  // End the ncurses mode
@@ -38,6 +57,13 @@ void center(char *title)
     rows =  rows / 2 - 1;
     mvaddstr(rows, indent, title);
     refresh();
-
 }
 
+void welcomeScreen()
+{
+    attron(A_BOLD);
+    center("Hello, ready to lift some weights?");
+    refresh();
+    getch();
+    return;
+}
