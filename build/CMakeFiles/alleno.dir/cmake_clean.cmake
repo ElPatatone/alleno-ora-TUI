c3d540813d2903801f3d4e063d31c12a3821f455
ElPatatone <rahman.hm2002@gmail.com>
2023-07-06 17:30:05 +0100

    feat: change the build system to use Cmake

---
+++ build/CMakeFiles/alleno.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/alleno.dir/main.c.o"
+  "CMakeFiles/alleno.dir/main.c.o.d"
+  "alleno"
+  "alleno.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/alleno.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/build.sh
@@ -0,0 +1,3 @@
+cmake -S ../src
+make
+
